e78e324245285bcf486e3164f00dba1e0f42026b
gonsva <50620041+gonsva@users.noreply.github.com>
2019-05-28 14:52:32 +0100

    Add files via upload

---
+++ TT.py
@@ -0,0 +1,6 @@
+TT= int(input("Times Table: "))
+LI= int(input("Times Table Till: "))
+A=1
+while A<=LI:
+	print(TT,"X",A, "=", (TT*A))
+	A=A+1 
---
+++ countthecharacters.py
@@ -0,0 +1,8 @@
+msg=input("Enter any message: ")
+Q=input("What are you looking for: ")
+
+
+if msg==Q:
+	Q=msg.count(Q)
+	
+print ("There are", msg.count(Q), Q)
